63f7aeb902ab61e4502dc74f7734ae8d2f7d91d8
Dan Valinotti <dan_valinotti@colpal.com>
2021-05-19 11:03:58 -0400

    move around atomico files

--- atomico-element/src/site.js
+++ atomico-element/src/site.js
@@ -1,7 +1,3 @@
-import { Hello } from "./components/hello.jsx";
-import { Brand } from "./components/brand.jsx";
 import { DvClipboardCopy } from "./components/dv-clipboard-copy";
 
-customElements.define("atomico-hello", Hello);
-customElements.define("atomico-brand", Brand);
 customElements.define("dv-clipboard-copy", DvClipboardCopy);
